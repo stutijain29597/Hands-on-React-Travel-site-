4d689f79bb1be3d4fcb32f62fd80cacbbc3a0b22
Stuti <stutijain2597@gmail.com>
2020-09-03 21:28:58 +0530

    adding sample header to site

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './UserList.css';
 import UserItem from './UserItem';
-
+import Card from '../../shared/components/UiElements/Card';
 const UserList = props => {
    if(props.items.length == 0) {
        return (<div className="center">
+           <Card>
            <h2>No Users Found.</h2>
+           </Card>
        </div>
        );
    }
